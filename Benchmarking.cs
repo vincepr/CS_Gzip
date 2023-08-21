42b9bca3e70324dd0ef800388305cd87323f92b7
probstv <probstv@PC7648>
2023-08-21 08:58:05 +0200

    init

--- Benchmarking.cs
+++ Benchmarking.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace CS_Gzip
 {
+    [MemoryDiagnoser]
     internal class Benchmarking
     {
     }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using CS_Gzip;
+
 Console.WriteLine("Hello, World!");
+string[] cmdArgs = new [] { "../../../test.txt.gz", "outfile.txt" }; 
+string result = GzipDecompress.GzipRun(cmdArgs);
+Console.WriteLine(result);
